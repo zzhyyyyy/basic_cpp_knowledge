c18d1033ac3ed6cdfb2a0f65e193978e992f42e5
zzhyyyyy <2685922758@qq.com>
2025-09-20 18:41:52 +0800

    lock

--- Async/mutex.cpp
+++ Async/mutex.cpp
@@ -5,8 +5,9 @@
 std::mutex mtx;//lock、unlock、try_lock
 void create(const int N,int &target)
 {
-    std::lock_guard<std::mutex>lock(mtx);//lock_guard就是在获取锁并自动管理锁的释放
+    //std::lock_guard<std::mutex>lock(mtx);//lock_guard就是在获取锁并自动管理锁的释放
     //等价于 std::lock_guard lock(mtx);
+    mtx.lock();
     for(int i=0;i<N;i++) target++;
     mtx.unlock();//虽然guard_lock可以自动管理锁 但也可由用户自己解锁
 }
@@ -14,15 +15,15 @@ void sub(const int N,int &target)
 {
     std::unique_lock<std::mutex>lock(mtx);
     for(int i=0;i<N;i++) target--;
-    mtx.unlock();
+    //mtx.unlock();
 }
 int main()
 {   
     const int N = 1000000;
     auto fun =[](){std::cout<<"Thread Ready"<<std::endl;};
     std::thread m(fun);
-    auto mm = std::move(m);
-    //if(m.joinable()) m.join();
+    if(m.joinable()) m.join();
+    std::thread mm(fun);
     if(mm.joinable())mm.join();
     int target=0;
     std::thread t(create,N,std::ref(target));  //thread源码(callback Function,Args&& ... __args)
